f41eeb9fb602dd1fb16ad2e4fcd9ec1107d2bfc3
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-08-07 17:34:00 +0530

    Get buttons in js variables

---
+++ main.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousOperandText = document.querySelector("[data-prev-operand]");
+const currOperandText = document.querySelector("[data-current-operand]");
