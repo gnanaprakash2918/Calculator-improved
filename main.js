2ed94c5cde6f17dc22967ff7622057dd06b0f90b
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-08-07 17:46:52 +0530

    Add constructor to calc class

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+class Calculator {
+  constructor(previousOperandText, currOperandText) {
+    this.previousOperandText = previousOperandText;
+    this.currOperandText = currOperandText;
+  }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
